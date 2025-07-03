66dddf5c7426ba4941c3ae37965475107dc951e4
muembiahub <jonathanmuembia3@gmail.com>
2025-07-03 22:04:26 +0200

    second

--- scripts/test.js
+++ scripts/test.js
@@ -5,3 +5,9 @@ navButton.addEventListener('click', () => {
     navMenu.classList.toggle('show');
 });
 const navMenu = document.querySelector('#nav-menu');
+lastModified = new Date(document.lastModified);
+document.querySelector('#last-modified').textContent = `${lastModified.toLocaleDateString()} ${lastModified.toLocaleTimeString()}`;
+const currentYear = new Date().getFullYear();
+document.querySelector('#current-year').textContent = currentYear;
+const footer = document.querySelector('footer');
+footer.innerHTML = footer.innerHTML.replace('2023', currentYear);
