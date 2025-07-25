61bc3facd0c50568efd9399d655fe1336e27541f
muembiahub <jonathanmuembia3@gmail.com>
2025-07-25 15:37:54 +0200

    cool

---
+++ chamber/scripts/footer.js
@@ -0,0 +1,108 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+
+
+<h3>NP Membership</h3>
+    <a href="#npModal" onclick="document.getElementById('npModal').showModal()">View Benefits</a>
+  </div>
+  <div class="membership">
+    <h3>Bronze Membership</h3>
+    <a href="#bronzeModal" onclick="document.getElementById('bronzeModal').showModal()">View Benefits</a>
+  </div>
+  <div class="membership">
+    <h3>Silver Membership</h3>
+    <a href="#silverModal" onclick="document.getElementById('silverModal').showModal()">View Benefits</a>
+  </div>
+  <div class="membership">
+    <h3>Gold Membership</h3>
+    <a href="#goldModal" onclick="document.getElementById('goldModal').showModal()">View Benefits</a>
+  </div>
+
+
+
+
+
+
+
+
+
+const address = [
+    {
+        name: "Kazidomo Chamber of Commerce",
+        address: "546 Industrial Way, Lubumbashi, ID 576440",
+        phone: "(243) 83170-1234",
+        website: "https://www.kazidomo.com",
+        email: "info@kazidomo.com"
+    }
+];
+
+
+
+
+
+
+
+
+// The data is rendered into the HTML structure of the directory page using JavaScript.
+// socialIcons is an array of objects, each representing a social media platform with its URL and icon.
+// It is used to display social media links on the directory page.
+const socialIcons = [
+    {
+        platform: "Facebook",
+        url: "https://facebook.com/kazidomo",
+        icon: "images/facebook.svg"
+    },
+    {
+        platform: "Twitter",
+        url: "https://twitter.com/kazidomo",
+        icon: "images/linkedin.svg"
+    },
+    {
+        platform: "Instagram",
+        url: "https://instagram.com/kazidomo",
+        icon: "images/instagram.svg"
+    }
+];
+
+// This script dynamically populates the directory page with data from the provided arrays and objects.
+// It uses the DOMContentLoaded event to ensure the HTML is fully loaded before executing the script
+// and renders the address, news, events, weather, world time zones, social media icons, and copyright information.
+// It also includes the last updated date of the document.
+document.addEventListener('DOMContentLoaded', () => {
+    // Address
+    const addressContainer = document.querySelector('.address');
+    addressContainer.innerHTML = address.map(item => `
+        <p><h4>${item.name}</h4>
+        <p>${item.address}<br>
+        <strong>Phone:</strong> ${item.phone}<br>
+        <strong>Email:</strong> <a href="mailto:${item.email}">${item.email}</a><br>
+        <strong>Website:</strong> <a href="${item.website}" target="_blank">${item.website}</a></p>
+    `).join('');
+
+
+    // Social Icons
+    const socialIconsContainer = document.querySelector('.social-icons');
+    socialIconsContainer.innerHTML = `
+        ${socialIcons.map(icon => `
+            <a href="${icon.url}" target="_blank">
+                <img src="${icon.icon}" alt="${icon.platform} icon" style="width:32px;height:32px;">
+            </a>
+        `).join('')}
+    `;
+
+    // Copyright and Last Updated
+    const copyrightContainer = document.querySelector('.copyright');
+    const currentYear = new Date().getFullYear();
+    if (copyrightContainer) {
+        copyrightContainer.innerHTML = `&copy; ${currentYear} Kazidomo . <br>
+         All rights reserved.<br>
+        Powered by <a href="https://www.byu.edu" target="_blank">BYU</a><br>
+        Last updated: <span id="last-modified">${document.lastModified}</span>`;
+    }
+});
